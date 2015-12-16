05570aa8971cdfc876d8cfcf9c727136d868162a
igork750 <igork750@gmail.com>
2015-12-16 21:17:11 +0200

    football api

---
+++ PycharmProjects/untitled1/football.py
@@ -0,0 +1,18 @@
+import requests
+# import AttrDict
+import pprint
+
+url = 'http://api.football-data.org/v1/soccerseasons/?season=2015'
+
+class ApiClient(object):
+    def __init__(self, url, key):
+        self.url = url
+        self.key = key
+
+response = requests.get(url)
+response1 = response.json()
+
+pprint.pprint(response)
+pprint.pprint(response1)
+
+response1('year')
